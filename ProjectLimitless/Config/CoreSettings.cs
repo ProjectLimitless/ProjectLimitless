dbd41306f1672de60c0d18eca8a77ad365071c0d
Donovan Solms <donovan.solms@gmail.com>
2016-11-21 22:32:17 +0200

    Working TOML config merge as test

---
+++ ProjectLimitless/Config/CoreSettings.cs
@@ -0,0 +1,47 @@
+﻿/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limitless.Config
+{
+    /// <summary>
+    /// CoreSettings holds the structure of the configuration TOML files.
+    /// </summary>
+    public class CoreSettings
+    {
+        ///TODO: Split into proper files and classes
+        public string Test { get; set; }
+        public OwnerSettings Owner { get; set; } = new OwnerSettings();
+        public UpdatesSettings Updates { get; set; } = new UpdatesSettings();
+
+        public class OwnerSettings
+        {
+            public string Name { get; set; }
+        }
+
+        public class UpdatesSettings
+        {
+            public UpdateSetting Primary { get; set; } = new UpdateSetting();
+            public UpdateSetting Secondary { get; set; } = new UpdateSetting();
+
+            public class UpdateSetting
+            {
+                public string Host { get; set; }
+            }
+        }
+    }
+}
--- ProjectLimitless/Program.cs
+++ ProjectLimitless/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿
+using Nett.Coma;
+/** 
+* This file is part of Project Limitless.
+* Copyright © 2016 Donovan Solms.
+* Project Limitless
+* https://www.projectlimitless.io
+* 
+* Project Limitless is free software: you can redistribute it and/or modify
+* it under the terms of the Apache License Version 2.0.
+* 
+* You should have received a copy of the Apache License Version 2.0 with
+* Project Limitless. If not, see http://www.apache.org/licenses/LICENSE-2.0.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +24,19 @@ namespace ProjectLimitless
     {
         static void Main(string[] args)
         {
+            ///TODO: Test file merging
+            var appSource =  ConfigSource.CreateFileSource("conf/Core.toml");
+            var userSource = ConfigSource.CreateFileSource("conf/User.toml");
+            var merged = ConfigSource.Merged(appSource, userSource); // order important here
+
+            // merge both TOML files into one settings object
+            Config<Limitless.Config.CoreSettings> settings = Config.Create(() => new Limitless.Config.CoreSettings(), merged);
+            Console.WriteLine("test: {0}", settings.Get(s => s.Test));
+            Console.WriteLine("Owner.Name: {0}", settings.Get(s => s.Owner.Name));
+            Console.WriteLine("Updates.Primary.Host: {0}", settings.Get(s => s.Updates.Primary.Host));
+            Console.WriteLine("Updates.Secondary.Host: {0}", settings.Get(s => s.Updates.Secondary.Host));
+
+            Console.ReadLine();
         }
     }
 }
